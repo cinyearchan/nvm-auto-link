d49657a36b4c5842b6ccae7367819776e794b683
cinyearchan <cinyearchan@gmail.com>
2021-12-15 16:18:22 +0800

    update

--- nvm-auto-link.plugin.zsh
+++ nvm-auto-link.plugin.zsh
@@ -5,4 +5,9 @@ fi
 
 #ln -s $(which node) /usr/local/bin/node
 #ln -s  /usr/local/bin/npm
-echo "nvm auto link"
+#echo "nvm auto link"
+if ! type $NVM_BIN > /dev/null; then
+  ln -s "$NVM_BIN/node" "/usr/local/bin/node"
+  ln -s "$NVM_BIN/npm" "/usr/local/bin/npm"
+  ln -s "$NVM_BIN/npx" "/usr/local/bin/npx"
+fi
